43e74508abfaca25ab56699ab87ce1a44c34a1d5
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2025-01-26 11:56:28 +0100

    add video carousrel

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,6 +1,7 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 import { rightImg, watchImg } from "../utils"
+import VideoCarousel from "./VideoCarousel"
 
 // import VideoCarousel from './VideoCarousel';
 
@@ -27,7 +28,7 @@ const Highlights = () => {
             </p>
           </div>
         </div>
-
+        <VideoCarousel />
       </div>
     </section>
   )
